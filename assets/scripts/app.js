3963e9bd396eaae7d69e3a07ec954ec299c6392b
root <root@DESKTOP-D4U004G.localdomain>
2020-01-27 14:05:24 -0500

    strong attack and heal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,33 +1,98 @@
 const ATTACK_VALUE = 10;
 const STRONG_VALUE = 20;
-const HEAL_VALUE = 15;
+const MONSTER_ATTACK = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 
-let currentMonsterHealth;
-let currentPlayerHealth;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 
 
 adjustHealthBars(chosenMaxLife);
 
 
+function playerAttack(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === 'STRONG_ATTACK') {
+        maxDamage = STRONG_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+    
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK);
+    currentPlayerHealth -= playerDamage;
+    
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert("you won");
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert("you lost");
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert('draw');
+    }
+    
+}
 
 function attack() {
-    damage = dealMonsterDamage(ATTACK_VALUE)
-    currentMonsterHealth -= damage
- 
+    playerAttack('ATTACK');
+    
+    // const damage = dealMonsterDamage(ATTACK_VALUE);
+    // currentMonsterHealth -= damage;
+    
+    // const playerDamage = dealPlayerDamage(MONSTER_ATTACK);
+    // currentPlayerHealth -= playerDamage;
+    
+    // if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    //     alert("you won");
+    // } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    //     alert("you lost");
+    // } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    //     alert('draw');
+    // }
 }
+
+
 
 function strongAttack() {
-    damage = dealMonsterDamage(STRONG_VALUE)
-    currentMonsterHealth -= damage
+    playerAttack('STRONG_ATTACK');
+    //  damage = dealMonsterDamage(STRONG_VALUE)
+    //  currentMonsterHealth -= damage;
+    
+    //  const playerDamage = dealPlayerDamage(MONSTER_ATTACK);
+    //  currentPlayerHealth -= playerDamage;
+    //  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    //     alert("you won");
+    // } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    //     alert("you lost");
+    // } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    //     alert('draw');
+    // }
 }
-// function strongAttack() {
 
-// }
-// function heal() {
+function heal() {
+    let healthValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        
+        healthValue = chosenMaxLife-currentPlayerHealth;
+        
+        currentPlayerHealth += healthValue;
+        increasePlayerHealth(healthValue);
+        alert(`you can only heal ${healthValue}`);
+    
+    } else {
 
-// }
+       increasePlayerHealth(HEAL_VALUE);
+    
+        currentPlayerHealth += healthValue;
+        alert(`you were healed ${healthValue}`)
+    }
+    playerDamage = dealPlayerDamage(MONSTER_ATTACK);
+    currentPlayerHealth -=playerDamage;
+
+
+}
 
 // function log() {
 
@@ -39,4 +104,4 @@ function strongAttack() {
 attackBtn.addEventListener('click', attack);
 strongAttackBtn.addEventListener('click', strongAttack);
 healBtn.addEventListener('click', heal);
-logBtn.addEventListener('click',log);
+logBtn.addEventListener('click', log);
