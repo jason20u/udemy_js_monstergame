5704cdfdc77326d6b93a864169e30075a9a7fb4b
root <root@DESKTOP-D4U004G.localdomain>
2020-02-04 00:37:49 -0500

    created if else if blocks for Logging of attack/heal/gameover events

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,11 +6,11 @@ const HEAL_VALUE = 20;
 
 const MODE_ATTACK = 'ATTACK'; // MODE_ATTACK = 0
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK'; //MODE_STRONG_ATTACK = 1
-const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';  // gave these strings because in array, it would've outputted numbers.  now it will display these strings to associate the output.
 const LOG_EVENT_STRONG_ATTACK = 'STRONG_ATTACK';
 const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_HEAL = 'HEAL';
-const LOG_EVENT_GAME_OUTCOME = 'GAME_OUTCOME';
+const LOG_EVENT_GAME_OVER = 'GAME_OUTCOME';
 const userInputLife = prompt('enter max life for battle.', '100');
 
 let chosenMaxLife = parseInt(userInputLife);
@@ -38,26 +38,66 @@ adjustHealthBars(chosenMaxLife);
 
 
 // write all activities events in vieable log
-function writeToLog(eventt, valuee, target, playerLogHealth, monsterLogHealth) {
+function writeToLog(eventt, valuee, monsterHealth, playerHealth) {
+    //function writeToLog(eventt, valuee, target, playerLogHealth, monsterLogHealth) {
     let logEntry;
 
     if (eventt === LOG_EVENT_PLAYER_ATTACK) {
         logEntry = {
-            event: eventt,  //notation for storing EVENT within a variable
+            event: eventt,  //notation for storing EVENT within a variable - objects
             value: valuee,
-            target: monster,
-            playerLogHealth: 
+            target: 'MONSTER',
+            playerLogHealth: playerHealth,
+            monsterLogHealth: monsterHealth
         };
-        battleLog.push(logEntry);
-    
-    } else if (event === LOG_EVENT_MONSTER_ATTACK) {
-        battleLog += event;
-    } else if (event === LOG_EVENT_HEAL) {
-        battleLog += event;
-    } else if (event === LOG_EVENT_GAME_OUTCOME) {
-        battleLog += event;
+        // battleLog.push(logEntry); -- Moved to bottom outside if statements but inside function.  thank way y ou don't have to repeat
+    } else if (eventt === LOG_EVENT_STRONG_ATTACK) {
+        logEntry = {
+            event: eventt,  //notation for storing EVENT within a variable - objects
+            value: valuee,
+            target: 'MONSTER',
+            playerLogHealth: playerHealth,
+            monsterLogHealth: monsterHealth
+        };
+        // battleLog.push(logEntry);
+    } else if (eventt === LOG_EVENT_MONSTER_ATTACK) {
+        logEntry = {
+            event: eventt,  //notation for storing EVENT within a variable - objects
+            value: valuee,
+            target: 'PLAYER',
+            playerLogHealth: playerHealth,
+            monsterLogHealth: monsterHealth
+        };
+        // battleLog.push(logEntry);
+    } else if (eventt === LOG_EVENT_HEAL) {
+        logEntry = {
+            event: eventt,  //notation for storing EVENT within a variable - objects
+            value: valuee,
+            target: 'HealPlayer',
+            playerLogHealth: playerHealth,
+            monsterLogHealth: monsterHealth
+        };
+        // battleLog.push(logEntry);
+    } else if (eventt === LOG_EVENT_GAME_OVER) {
+        logEntry = {
+            event: eventt,  //notation for storing EVENT within a variable - objects
+            value: valuee, //for this event, might be changing valuee to represent the output from endRound()
+  
+            playerLogHealth: playerHealth,
+            monsterLogHealth: monsterHealth
+        };
+        //battleLog.push(logEntry);
     }
+    battleLog.push(logEntry);
 
+    // } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+    //     battleLog += event;
+    // } else if (event === LOG_EVENT_HEAL) {
+    //     battleLog += event;
+    // } else if (event === LOG_EVENT_GAME_OUTCOME) {
+    //     battleLog += event;
+    // }
+   
 }
 
 
@@ -89,7 +129,8 @@ function endRound() {
     const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_VALUE);
     currentPlayerHealth -= playerDamage;
-    writeToLog(LOG_EVENT_MONSTER_ATTACK);
+    writeToLog(LOG_EVENT_MONSTER_ATTACK, playerDamage);
+
     //const initialPlayerHealth = currentPlayerHealth
     // const playerDamage = dealPlayerDamage(MONSTER_VALUE);
     // currentPlayerHealth -= playerDamage;
@@ -135,7 +176,7 @@ function playerAttack(mode) {
    
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage;
-    writeToLog(LOG_EVENT_PLAYER_ATTACK)
+    writeToLog(LOG_EVENT_PLAYER_ATTACK, damage);
     endRound();
  
 }
